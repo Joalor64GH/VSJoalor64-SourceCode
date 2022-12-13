06955c4391bade83bb23282e3303fad73187ae21
Joalor64 <rhgarcon@gmail.com>
2022-12-12 19:36:36 -0500

    nvm these don't even work

--- example_mods/awards/data/awards/songStoppa.lua
+++
@@ -1,9 +0,0 @@
-function onStartCountdown()
-    return Function_Stop;
-end
-
-function onUpdatePost()
-    if(getPropertyFromClass('flixel.FlxG','keys.justPressed.ESCAPE'))then
-        endSong()
-    end
-end
